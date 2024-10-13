fbf0b738252beead13e9606c533338c270ded16f
Paulerpro <nrpaulsilas@gmail.com>
2024-10-13 11:39:10 +0100

    added extra fields to default usermodel/connected PostgresQL

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
 
-# register ur apps
-
 class RegisterView(generics.CreateAPIView):
     queryset = ""
     serializer_class = ""
