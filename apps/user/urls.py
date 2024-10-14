a32607eefc0aa55c4996c3ed6a927dbd03aa9dd9
Paulerpro <nrpaulsilas@gmail.com>
2024-10-14 22:04:07 +0100

    user setup

---
+++ apps/user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+# from django.contrib.auth.views import LoginView, LogoutView
+
+from apps.user.views import RegisterView, LoginView
+
+urlpatterns = [
+    path('register-user/', RegisterView.as_view(), name="register-user"),
+    path('login-user/', LoginView.as_view(), name="login-user"),
+    # path('logout-user/', LogoutView.as_view(), name="logout-user"),
+]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from apps.user.serializers import UserProfileSerilaizer
 
 class RegisterView(generics.CreateAPIView):
-    queryset = ""
-    serializer_class = ""
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerilaizer
 
-class LoginView():
-    pass
+class LoginView(APIView):
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return Response({'message': 'User logged in'}, 200)
+        return Response({"User doesn't exist"}, 400)
+
